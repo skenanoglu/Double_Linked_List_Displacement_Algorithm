e9b828c426d6db00b0dacd76996662e5355a9f6d
sahin kenanoglu <kenanoglu115@gmail.com>
2021-01-23 23:57:41 +0300

    Proje dosyası ekle.

---
+++ Node_Yer_Degistirme/DoubleLinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Node_Yer_Degistirme
+{
+    class DoubleLinkedList<T>
+    {
+        public Node first;
+        private Node last;
+        public class Node
+        {
+            public Node(T data)
+            {
+                this.data=data  ;
+            }
+            public Node next;
+            public Node prev;
+            public T data;
+        }
+         public void AddLast(T veri)
+         {
+             Node newnode = new Node(veri);
+             if (first==null)
+             {
+                 first = last= newnode;
+             }
+             else
+             {
+                 last.next = newnode;
+                 newnode.prev = last;
+                 last = newnode;
+             }
+         }
+
+         public void Showlist()
+         {
+            Node trvl = first;
+            while (trvl != null)
+            {
+                Console.WriteLine(trvl.data);
+                trvl = trvl.next;
+            }
+
+            /* Node trvl = last;
+             while (trvl != null)
+             {
+                 Console.WriteLine(trvl.data);
+                 trvl = trvl.prev;
+             }*/
+        }
+
+         public void YerDegistir(Node b,Node c)
+         {
+
+             //Node p,Node k olursa....
+            /* p.prev.next = k;
+               k.prev = p.prev;
+               k.next.prev = p;
+               p.next = k.next;
+               k.next = p;
+               p.prev = k;   */
+
+               b.next = c.next;
+               c.prev = b.prev;
+               c.next.prev = b;
+               c.next = b;
+               b.prev.next = c;
+               b.prev = c;
+
+        }
+
+
+
+
+    }
+   
+}
---
+++ Node_Yer_Degistirme/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Node_Yer_Degistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleLinkedList<int> exmp = new DoubleLinkedList<int>();
+            exmp.AddLast(15);
+            exmp.AddLast(16);
+            exmp.AddLast(17);
+            exmp.AddLast(18);
+            exmp.AddLast(19);
+            exmp.AddLast(120);
+
+            exmp.Showlist();
+            exmp.YerDegistir(exmp.first.next, exmp.first.next.next);
+            Console.WriteLine("**********************************");
+            exmp.Showlist();
+        }
+    }
+}
